92254e43e2fbf4409eee36fc0fa4d5b992455144
maxh33 <contact@maxhaider.dev>
2024-11-19 11:12:17 -0300

    feat: avatar cocomponents

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,16 @@
+import Avatar from '../../components/Avatar'
+import Paragraph from '../../components/Paragraph'
 import Title from '../../components/Title'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/maxh33.png" />
+    <Avatar />
     <Title fontSize={20}>Maxh33</Title>
+    <Paragraph type="secondary" fontSize={16}>
+      Maxh33
+    </Paragraph>
+    <Paragraph fontSize={12}>Full Stack Developer</Paragraph>
+    <button>Change Theme</button>
   </aside>
 )
 
