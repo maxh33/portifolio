86282b7b0a1d7b4f713cb8ed32afd30a529a2ec7
maxh33 <contact@maxhaider.dev>
2024-11-20 15:16:03 -0300

    style: enhance about me content display

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Title from '../../components/Title'
 import Paragraph from '../../components/Paragraph'
 import { GithubStats } from './styles'
@@ -5,12 +6,27 @@ import { GithubStats } from './styles'
 const About = () => (
   <>
     <section>
-      <Title fontSize={16}>About</Title>
+      <Title fontSize={16}>About me</Title>
       <Paragraph type="secondary">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque,
-        pariatur aliquam nesciunt veritatis magni eius nam voluptatibus nostrum
-        harum enim, modi reprehenderit amet laboriosam blanditiis eum repellat
-        necessitatibus aperiam consectetur.
+        My journey in technology began with certifications in hardware and
+        software, evolving to experiences in web development and e-commerce. As
+        owner of Joiasmax, I developed Web skills and sales optimization.
+      </Paragraph>
+      <Paragraph type="secondary">
+        Currently, I am a Full Stack Python developer, focusing on creating web
+        solutions and microservices. My projects on GitHub reflect this journey,
+        with emphasis on developing applications in Django, scripts with Python
+        for data processing and back-end integrations.
+      </Paragraph>
+      <Paragraph type="secondary">
+        Key Skills: Python, Django, PostgreSQL, Docker, LLMs, Google Cloud
+        Platform (GCP), API Integration, GTM, and Analytics, Full-stack
+        development (front-end and back-end), Web development (JavaScript,
+        TypeScript, React), WordPress, WooCommerce, Elementor, English C1
+        (Advanced)
+      </Paragraph>
+      <Paragraph type="secondary">
+        Contact: +55 43 9 9158-1772 | contact@maxhaider.dev
       </Paragraph>
       <GithubStats>
         <img src="https://github-readme-stats.vercel.app/api?username=maxh33&show_icons=true&theme=apprentice&include_all_commits=true&count_private=true" />
--- src/containers/Projects/index.tsx
+++ src/containers/Projects/index.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Title from '../../components/Title'
 import Repositories from '../../components/Project/index'
 
-import { List } from './styles'
-
 const Projects = () => (
   <section>
     <Title fontSize={16}>My GitHub Repositories</Title>
